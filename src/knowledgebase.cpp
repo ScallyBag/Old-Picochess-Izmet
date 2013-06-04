583cde0ea9690cbe3242ee222d0506e989f57f90
Jean-Francois Romang <jeanfrancois.romang@gmail.com>
2013-06-04 07:26:59 -0400

    Add KBNK knowledge

--- src/knowledgebase.cpp
+++ src/knowledgebase.cpp
@@ -39,6 +39,29 @@ namespace {
       1, 1, 2, 3, 3, 2, 1, 1,
       0, 1, 2, 3, 3, 2, 1, 0
     };
+    
+  const int bishop_corner_dist[2][64]={
+    {
+      0, 1, 2, 3, 4, 5, 6, 7,
+      1, 1, 2, 3, 4, 5, 6, 6,
+      2, 2, 2, 3, 4, 5, 5, 5,
+      3, 3, 3, 3, 4, 4, 4, 4,
+      4, 4, 4, 4, 3, 3, 3, 3,
+      5, 5, 5, 4, 3, 2, 2, 2,
+      6, 6, 5, 4, 3, 2, 1, 1,
+      7, 6, 5, 4, 3, 2, 1, 0
+    },
+    {
+      7, 6, 5, 4, 3, 2, 1, 0,
+      6, 6, 5, 4, 3, 2, 1, 1,
+      5, 5, 5, 4, 3, 2, 2, 2,
+      4, 4, 4, 4, 3, 3, 3, 3,
+      3, 3, 3, 3, 4, 4, 4, 4,
+      2, 2, 2, 3, 4, 5, 5, 5,
+      1, 1, 2, 3, 4, 5, 6, 6,
+      0, 1, 2, 3, 4, 5, 6, 7
+    }
+  };
 
   // Get the material key of a Position out of the given endgame key code
   // like "KBPKN". The trick here is to first forge an ad-hoc fen string
@@ -88,7 +111,29 @@ namespace {
       return false;
 
     result = ( popcount<Max15>(bishops & BlackSquares)!=1 ) ? VALUE_DRAW // The endgame KBBK is drawn if the bishops cover squares of a single color only
-            : 2*BishopValueMg + 250 - 25*corner_dist[loserKSq] - 12*square_distance(loserKSq,winnerKSq);
+            : 2*BishopValueEg + 250 - 25*corner_dist[loserKSq] - 12*square_distance(loserKSq,winnerKSq);
+
+    v=(strongerSide == pos.side_to_move()) ? result : -result;
+    return true;
+  }
+  
+  template<Color strongerSide>
+  bool KBNK(const Position& pos, Value &v)
+  {
+    Color weakerSide=~strongerSide;
+    Value result;
+    Square winnerKSq = pos.king_square(strongerSide);
+    Square loserKSq = pos.king_square(weakerSide);
+    const Bitboard edges=FileBB[0]|FileBB[7]|RankBB[0]|RankBB[7];
+    const Bitboard corners=SquareBB[SQ_A1] | SquareBB[SQ_A8] |  SquareBB[SQ_H1] |  SquareBB[SQ_H8];
+    
+    if(   (pos.pieces(strongerSide) & pos.attacks_from<KING>(loserKSq)) // Don't use this function if weaker side king can capture a piece
+       || ((pos.pieces(weakerSide,KING)&edges) && SquareDistance[loserKSq][winnerKSq]<=2) // Dont't use this function is there is a stalemate risk
+       || (pos.pieces(strongerSide,KNIGHT)&corners) ) // Dont't use this function is there is a trapped knight risk
+      return false;
+
+    Color bishopSquareColor= (pos.pieces(strongerSide,BISHOP) & BlackSquares) ? BLACK : WHITE;
+    result = BishopValueEg + KnightValueEg - 50 - 25*bishop_corner_dist[bishopSquareColor][loserKSq] - 12*square_distance(loserKSq,winnerKSq);
 
     v=(strongerSide == pos.side_to_move()) ? result : -result;
     return true;
@@ -168,7 +213,8 @@ KnowledgeBases::KnowledgeBases()
 
   m[key("KBBK", WHITE)]=KBBK<WHITE>;
   m[key("KBBK", BLACK)]=KBBK<BLACK>;
-
+  m[key("KBNK", WHITE)]=KBNK<WHITE>;
+  m[key("KBNK", BLACK)]=KBNK<BLACK>;
 }
 
 

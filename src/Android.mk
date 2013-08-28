2550bea2f7f73ed2830f8f828aaaf49828908c72
Shivkumar Shivaji <sshivaji@gmail.com>
2013-08-28 01:32:22 -0700

    Almost there now, need alternative to pthread_cancel() for final compile

--- src/Android.mk
+++ src/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := stockfish
 LOCAL_SRC_FILES := \
 	benchmark.cpp	book.cpp	evaluate.cpp	misc.cpp	notation.cpp	search.cpp	tt.cpp \
-	bitbase.cpp	dgt.cpp		main.cpp	movegen.cpp	pawns.cpp	thread.cpp	uci.cpp \
+	bitbase.cpp	dgt.cpp  dgtnix.c		main.cpp	movegen.cpp	pawns.cpp	thread.cpp	uci.cpp \
 	bitboard.cpp	endgame.cpp	material.cpp	movepick.cpp	position.cpp	timeman.cpp	ucioption.cpp
 
 LOCAL_CFLAGS    := -I$(ANDROID_NDK)/sources/cxx-stl/stlport/stlport \
